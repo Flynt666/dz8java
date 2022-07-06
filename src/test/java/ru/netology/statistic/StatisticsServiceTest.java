fde199f991947cd1218e4c2a80acdbab22b01441
Борис Распутин <bang-kirov@bk.ru>
2022-07-06 21:27:09 +0300

    New test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
-    public class StatisticsServiceTest {
 
         @Test
         void findMax() {
@@ -17,6 +16,19 @@ public class StatisticsServiceTest {
             long actual = service.findMax(incomesInBillions);
 
             assertEquals(expected, actual);
+        }
+
+        @Test
+    void findMaxNonFirst() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+            long expected = 12;
+
+            long actual = service.findMax(incomesInBillions);
+
+            assertEquals(expected, actual);
+
         }
 
 }
